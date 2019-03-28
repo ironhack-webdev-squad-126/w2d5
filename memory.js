d5772a301201d97387349af1fa518c297e7a9e7f
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-28 14:53:55 +0100

    shufflingÄ

--- memory.js
+++ memory.js
@@ -1,1 +1,23 @@
-console.log('hello :')
+/* eslint-disable */
+
+document.querySelectorAll('.card').forEach(function(card, index) {
+    TweenMax.fromTo(
+        card,
+        1,
+        { x: getRandomInt(500, true), y: getRandomInt(500, true) },
+        {
+            x: 0,
+            y: 0,
+            ease: Back.easeOut,
+        }
+    )
+})
+
+function getRandomInt(max, negative) {
+    let randNum = Math.floor(Math.random() * max)
+    if (negative && Math.random() < 0.5) {
+        randNum *= -1
+    }
+
+    return randNum
+}
