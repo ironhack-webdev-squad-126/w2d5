d0daed86253e32c06c6a99c2a537640c7ed8f730
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-28 11:13:10 +0100

    multiple balls

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // TweenMax.to('.ball', 1.5, { y: 500 })
 // TweenMax.staggerTo('.ball', 1.5, { y: 500 }, 0.25)
 // TweenMax.staggerFromTo(
@@ -15,3 +16,14 @@
 //     { y: 500, scale: 1, opacity: 1, ease: Bounce.easeOut, yoyo: true, repeat: -1 },
 //     0.25
 // )
+
+// 1 Ball
+// document.querySelector('.ball').addEventListener('click', function(evt) {
+//     TweenMax.to('.ball', 2.5, { y: 500, ease: Elastic.easeIn.config(1, 0.3) })
+// })
+
+document.querySelectorAll('.ball').forEach(domBall => {
+    domBall.addEventListener('click', function() {
+        TweenMax.to(this, 2.5, { y: 500, ease: Bounce.easeOut })
+    })
+})
