96b7b874c9109130c65b3f56301032ae2f3fa878
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-29 10:32:20 +0100

    enable non live reload

--- birds.js
+++ birds.js
@@ -3,15 +3,16 @@ let img
 function setup() {
     createCanvas(640, 640)
     frameRate(60)
-    background(230)
-    img = loadImage('assets/bird.png')
+    img = loadImage(
+        'https://raw.githubusercontent.com/ironhack-webdev-squad-126/w2d5/master/assets/bird.png'
+    )
 }
 
 let ypos = 0
 
 function draw() {
     clear()
-    background(230)
+    background(240)
 
     fill(0)
     if (ypos > 640) ypos = 0
@@ -21,5 +22,5 @@ function draw() {
     } else {
         ypos += 1
     }
-    image(img, 300, ypos, img.width / 7, img.height / 7)
+    image(img, 300, ypos, img.width / 10, img.height / 10)
 }
