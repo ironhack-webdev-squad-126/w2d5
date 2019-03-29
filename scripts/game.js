a25af31fae5dc952d79cc82ee5396ed301381d9e
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-29 14:33:34 +0100

    score + speed* *

--- scripts/game.js
+++ scripts/game.js
@@ -6,20 +6,48 @@ class Game {
         this.obstacles = []
         this.bird = new Bird()
         this.gameOver = false
-        this.count = 0
+        this.score = 0
+        this.obstacleVelocity = -7
+        this.spawnIntervalTimer = 1500
     }
 
     setup() {
         createCanvas(GAME_WIDTH, GAME_HEIGHT)
         frameRate(60)
         this.bird.setup()
-
+        this.setSpawnInterval()
+        this.setScoreInterval()
         setInterval(
             function() {
-                this.obstacles.push(new Obstacle())
-                if (this.obstacles.length > 3) this.obstacles.shift()
+                this.obstacleVelocity -= 1
+                this.setSpawnInterval()
             }.bind(this),
-            1350
+            1500
+        )
+    }
+
+    setSpawnInterval() {
+        this.spawnIntervalTimer -= 100
+        if (this.spawnInterval) {
+            clearInterval(this.spawnInterval)
+        }
+
+        this.spawnInterval = setInterval(
+            function() {
+                this.obstacles.push(new Obstacle())
+                // if (this.obstacles.length > 3) this.obstacles.shift()
+            }.bind(this),
+            this.spawnIntervalTimer
+        )
+    }
+
+    setScoreInterval() {
+        this.scoreInterval = setInterval(
+            function() {
+                this.score++
+                document.querySelector('h1').innerHTML = `Score: ${this.score}`
+            }.bind(this),
+            1000
         )
     }
 
@@ -39,5 +67,6 @@ class Game {
 
     over() {
         this.gameOver = true
+        clearInterval(this.scoreInterval)
     }
 }
--- scripts/obstacle.js
+++ scripts/obstacle.js
@@ -9,13 +9,12 @@ class Obstacle {
 
         this.topEnd = gapPos - GAP_OPENING / 2
         this.botY = gapPos + GAP_OPENING / 2
-
         this.x = GAME_WIDTH
     }
 
     draw() {
         fill(0)
-        this.x -= 7
+        this.x += game.obstacleVelocity
         rect(this.x, 0, this.width, this.topEnd)
         rect(this.x, this.botY, this.width, GAME_HEIGHT - this.botY)
         this.checkCollision()
