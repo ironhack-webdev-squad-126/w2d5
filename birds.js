3e5b0b025a6f6686e90f3735ee19423e935eb67b
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-29 10:07:29 +0100

    circles moving

--- birds.js
+++ birds.js
@@ -1,1 +1,23 @@
 /* eslint-disable */
+function setup() {
+    createCanvas(640, 640)
+    frameRate(60)
+    background(230)
+}
+
+let ypos = 0
+let yposTwo = 0
+
+function draw() {
+    clear()
+    background(230)
+
+    if (ypos > 640) ypos = 0
+    if (yposTwo > 640) yposTwo = 0
+
+    fill(0)
+
+    ellipse(400, ypos++, 80, 80)
+    yposTwo += 2
+    ellipse(300, yposTwo, 80, 80)
+}
